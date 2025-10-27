f48dfa287224599c475dedbe4850a8cdb0004301
Teo <teodoratoply07@gmail.com>
2025-10-27 18:41:53 +0200

    expression added

---
+++ ToyLanguage/src/model/expressions/ArithExp.java
@@ -0,0 +1,51 @@
+package model.expressions;
+
+import exceptions.MyException;
+import model.adt.MyIDictionary;
+import model.types.IntType;
+import model.values.IValue;
+import model.values.IntValue;
+
+public class ArithExp implements IExp {
+    private IExp e1;
+    private IExp e2;
+    private int op; // 1-plus, 2-minus, 3-star, 4-division
+
+    @Override
+    public IValue eval(MyIDictionary<String, IValue> tbl)  throws MyException{
+        IValue v1,v2;
+        v1= e1.eval(tbl);
+        if (v1.getType().equals(new IntType())) {
+            v2 = e2.eval(tbl);
+            if (v2.getType().equals(new IntType())) {
+                IntValue i1 = (IntValue)v1;
+                IntValue i2 = (IntValue)v2;
+                int n1,n2;
+                n1= i1.getValue();
+                n2 = i2.getValue();
+                if (op==1)  return new IntValue(n1+n2);
+                if (op ==2) return new IntValue(n1-n2);
+                if (op==3)  return new IntValue(n1*n2);
+                if (op==4)
+                    if(n2==0) throw new MyException("division by zero");
+                    else  return new IntValue(n1/n2);
+
+                throw new MyException("Invalid operator");
+            } else
+                throw new MyException("second operand is not an integer");
+        } else
+            throw new MyException("first operand is not an integer");
+    }
+
+    @Override
+    public String toString() {
+        String sign = switch (op){
+            case 1 -> "+";
+            case 2 -> "-";
+            case 3 -> "*";
+            case 4 -> "/";
+            default -> "?";
+        };
+        return e1.toString() + sign + e2.toString();
+    }
+}
---
+++ ToyLanguage/src/model/expressions/ValueExp.java
@@ -0,0 +1,23 @@
+package model.expressions;
+
+import exceptions.MyException;
+import model.adt.MyIDictionary;
+import model.values.IValue;
+
+public class ValueExp implements IExp {
+    private IValue value;
+
+    public ValueExp(IValue value) {
+        this.value = value;
+    }
+
+    @Override
+    public IValue eval(MyIDictionary<String, IValue> dict) throws MyException {
+        return value;
+    }
+
+    @Override
+    public String toString(){
+        return value.toString();
+    }
+}
