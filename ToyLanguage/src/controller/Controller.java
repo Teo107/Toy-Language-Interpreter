868f4b092c36709acfe399e3aced1729c1b70c63
Teo <teodoratoply07@gmail.com>
2025-10-28 14:57:51 +0200

    Controller added

---
+++ ToyLanguage/src/controller/Controller.java
@@ -0,0 +1,37 @@
+package controller;
+
+import exceptions.MyException;
+import model.PrgState;
+import model.adt.MyIStack;
+import model.statements.IStmt;
+import repository.IRepository;
+
+import java.sql.SQLOutput;
+
+public class Controller {
+    private IRepository repo;
+
+    public Controller(IRepository repo) {
+        this.repo = repo;
+    }
+
+    public PrgState oneStep(PrgState state) throws MyException {
+        MyIStack<IStmt> stk = state.getExeStack();
+
+        if (stk.isEmpty())
+            throw new MyException("prgstate stack is empty");
+        IStmt crtStmt = stk.pop();
+        return crtStmt.execute(state);
+    }
+
+    public void allStep() throws MyException {
+        PrgState prg = repo.getCrtPrg();
+        System.out.println("Initial state: " + prg);
+        while(!prg.getExeStack().isEmpty()){
+            oneStep(prg);
+            System.out.println("Current state: " + prg);
+        }
+        System.out.println("Final state: " + prg);
+    }
+
+}
